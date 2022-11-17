558ff39d9580ca5002ef41b864c4cd4d8db3b861
Jeevitha1920 <113667655+Jeevitha1920@users.noreply.github.com>
2022-11-17 15:26:02 +0530

    Update Flask_Integration.py

--- Flask_Integration.py
+++ Flask_Integration.py
@@ -1,1 +1,18 @@
 
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "Nao5NlrxCCWqpmFusPqFp8KCgT66q4gvH2IiJQFq_HcL"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE: manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}]}
+
+response_scoring = requests.post('https://eu-de.ml.cloud.ibm.com/ml/v4/deployments/9aecac79-02b8-4ad2-955c-ff801c4bc43c/predictions?version=2022-11-17', json=payload_scoring,
+ headers={'Authorization': 'Bearer ' + mltoken})
+print("Scoring response")
+print(response_scoring.json())
